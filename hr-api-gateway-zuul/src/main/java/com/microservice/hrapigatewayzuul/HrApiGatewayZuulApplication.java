0e2a1ecd9c4b56a46bcb9b3280f460b1acde6ff2
taai <taai@gft.com>
2023-04-30 14:35:10 -0300

    inclusao gateway

--- hr-api-gateway-zuul/src/main/java/com/microservice/hrapigatewayzuul/HrApiGatewayZuulApplication.java
+++ hr-api-gateway-zuul/src/main/java/com/microservice/hrapigatewayzuul/HrApiGatewayZuulApplication.java
@@ -1,17 +1,17 @@
-package com.microservice.hrpayroll;
+package com.microservice.hrapigatewayzuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@EnableEurekaClient
-@EnableFeignClients
 @SpringBootApplication
-public class HrPayrollApplication {
+@EnableZuulProxy
+@EnableEurekaClient
+public class HrApiGatewayZuulApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HrPayrollApplication.class, args);
+		SpringApplication.run(HrApiGatewayZuulApplication.class, args);
 	}
 
 }
--- hr-payroll/src/main/java/com/microservice/hrpayroll/entities/Payment.java
+++ hr-payroll/src/main/java/com/microservice/hrpayroll/entities/Payment.java
@@ -2,21 +2,47 @@ package com.microservice.hrpayroll.entities;
 
 import java.io.Serializable;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class Payment implements Serializable{
-
+public class Payment implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	private String name;
 	private Double dailyIncome;
 	private Integer days;
-	
+
+	public Payment() {
+	}
+
+	public Payment(String name, Double dailyIncome, Integer days) {
+		super();
+		this.name = name;
+		this.dailyIncome = dailyIncome;
+		this.days = days;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getDailyIncome() {
+		return dailyIncome;
+	}
+
+	public void setDailyIncome(Double dailyIncome) {
+		this.dailyIncome = dailyIncome;
+	}
+
+	public Integer getDays() {
+		return days;
+	}
+
+	public void setDays(Integer days) {
+		this.days = days;
+	}
+
 	public double getTotal() {
 		return days * dailyIncome;
 	}
