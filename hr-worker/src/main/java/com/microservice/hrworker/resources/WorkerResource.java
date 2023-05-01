cd284a25ca26650161fb67787ac24ada2f206cb2
taai <taai@gft.com>
2023-05-01 08:02:32 -0300

    iniciando hr-user

--- hr-worker/src/main/java/com/microservice/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/microservice/hrworker/resources/WorkerResource.java
@@ -5,6 +5,7 @@ import com.microservice.hrworker.repositories.WorkerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RefreshScope
 @RestController
 @RequestMapping("/workers")
 public class WorkerResource {
@@ -22,8 +24,17 @@ public class WorkerResource {
 	@Autowired
 	private Environment env;
 
+//	@Value("${test.config}")
+//	private String testConfig;
+
 	@Autowired
 	private WorkerRepository repository;
+
+	@GetMapping("/configs")
+	public ResponseEntity<Void> getConfigs(){
+		//logger.info("CONFIG = " + testConfig);
+		return ResponseEntity.noContent().build();
+	}
 	
 	@GetMapping
 	public ResponseEntity<List<Worker>> findAll(){
