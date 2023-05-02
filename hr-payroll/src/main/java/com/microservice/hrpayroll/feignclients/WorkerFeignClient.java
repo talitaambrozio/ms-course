d6224fad000192f101a83a4cf5344e93817bfe0d
taai <taai@gft.com>
2023-05-02 17:19:13 -0300

    configuracooes serv de config

--- hr-payroll/src/main/java/com/microservice/hrpayroll/feignclients/WorkerFeignClient.java
+++ hr-payroll/src/main/java/com/microservice/hrpayroll/feignclients/WorkerFeignClient.java
@@ -1,18 +1,17 @@
 package com.microservice.hrpayroll.feignclients;
 
+import com.microservice.hrpayroll.entities.Worker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import com.microservice.hrpayroll.entities.Worker;
-
 @Component
 @FeignClient(name = "hr-worker",  path="/workers")
 public interface WorkerFeignClient {
-	
-	@GetMapping("/{id}")
+
+	@GetMapping(value = "/{id}")
 	ResponseEntity<Worker> findById(@PathVariable Long id);
 
 }
